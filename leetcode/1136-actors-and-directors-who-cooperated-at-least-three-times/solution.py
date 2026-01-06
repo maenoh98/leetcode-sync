dc12f4fdf25a901ba47d29bdf781d40c35fb0a7d
maenoh98 <maenoh98@gmail.com>
2026-01-06 01:51:07 +0000

    [LeetCode Sync] Runtime - 263 ms (91.54%), Memory - 67.8 MB (43.37%)

---
+++ leetcode/1136-actors-and-directors-who-cooperated-at-least-three-times/solution.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    df = actor_director.groupby(['actor_id', 'director_id'])['timestamp'].nunique().reset_index(name='cnt')
+
+    return df[df['cnt'] >= 3][['actor_id', 'director_id']]
