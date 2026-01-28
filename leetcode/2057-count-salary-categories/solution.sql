eb0f125d5b0afdaf91282bf09b8f3df14d0dd032
maenoh98 <maenoh98@gmail.com>
2026-01-28 07:33:38 +0000

    [LeetCode Sync] Runtime - 1524 ms (51.39%), Memory - 0.0B (100.00%)

---
+++ leetcode/2057-count-salary-categories/solution.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+
+SELECT
+    c.category,
+    COUNT(DISTINCT account_id) AS accounts_count
+FROM (
+    SELECT 'Low Salary' AS category
+    UNION ALL
+    SELECT 'Average Salary'
+    UNION ALL
+    SELECT 'High Salary'
+) c
+LEFT JOIN (
+    SELECT  
+        account_id,
+        CASE
+            WHEN income < 20000 THEN 'Low Salary'
+            WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+            ELSE 'High Salary'
+        END AS category
+    FROM accounts
+) a
+ON c.category = a.category
+GROUP BY category
+;
