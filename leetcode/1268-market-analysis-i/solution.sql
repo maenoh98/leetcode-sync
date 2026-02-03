18f7969b8d77a40da50bc315e1cfc256d09c1fd1
maenoh98 <maenoh98@gmail.com>
2026-02-03 03:26:39 +0000

    [LeetCode Sync] Runtime - 1248 ms (37.90%), Memory - 0.0B (100.00%)

---
+++ leetcode/1268-market-analysis-i/solution.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT
+    u.user_id AS buyer_id,
+    u.join_date,
+    COALESCE(o.orders_in_2019, 0) AS orders_in_2019
+FROM users u
+LEFT JOIN (
+        SELECT
+            buyer_id,
+            COUNT(DISTINCT order_id) AS orders_in_2019
+        FROM orders
+        WHERE YEAR(order_date) = '2019'
+        GROUP BY buyer_id
+        ) o
+    ON u.user_id = o.buyer_id
+;
