a2b47662993abea1406cd0d5b975057bb0a84f4d
maenoh98 <maenoh98@gmail.com>
2026-02-12 04:35:37 +0000

    [LeetCode Sync] Runtime - 323 ms (70.88%), Memory - 68.6 MB (63.98%)

---
+++ leetcode/2041-the-latest-login-in-2020/solution.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def latest_login(logins: pd.DataFrame) -> pd.DataFrame:
+    con = logins['time_stamp'].dt.year == 2020
+
+    df = logins[con].copy()
+
+    df2 = df.groupby('user_id')['time_stamp'].max().reset_index(name='last_stamp')
+
+    return df2
