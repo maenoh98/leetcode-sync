3c59f0cfa48e8eb574daa01bcf303567123074f4
maenoh98 <maenoh98@gmail.com>
2026-02-21 10:13:27 +0000

    [LeetCode Sync] Runtime - 267 ms (78.60%), Memory - 0.0B (100.00%)

---
+++ leetcode/3816-dna-pattern-recognition-/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    *,
+    CASE WHEN dna_sequence LIKE 'ATG%' THEN 1 ELSE 0 END AS has_start,
+    CASE WHEN RIGHT(dna_sequence, 3) IN ('TAA', 'TAG', 'TGA') THEN 1 ELSE 0 END AS has_stop,
+    CASE WHEN dna_sequence LIKE '%ATAT%' THEN 1 ELSE 0 END AS has_atat,
+    CASE WHEN dna_sequence LIKE '%GGG%' THEN 1 ELSE 0 END AS has_ggg
+FROM samples
+;
